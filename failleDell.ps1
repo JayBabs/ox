d5edf2e944f7f103346ae1f10fdf2939f8c78e4e
JayBabs <jbabeu42@gmail.com>
2021-08-12 16:07:29 -0400

    DELL DSA-2021-088

---
+++ failleDell.ps1
@@ -0,0 +1,33 @@
+﻿#JB
+
+$root="c:\Users"
+$users = get-childitem -path $root -exclude administrator, public, administrateurs, ".Net v4.5", ".Net v4.5 Classic", DefaultAppPool
+
+$pathComplet = "C:\Windows\Temp\dbutil_2_3.sys"
+$FileExists = Test-Path $pathComplet
+
+if($FileExists -eq $True)
+
+{Write-Host "Ficher présent dans $pathComplet"}
+
+else
+
+{Write-Host "Fichier introuvable dans $pathComplet"}
+
+
+
+
+foreach ($user in $users)
+ { 
+ $pathComplet = "$user\APPDATA\Local\Temp\dbutil_2_3.sys"
+ $FileExists = Test-Path $pathComplet
+
+ If ($FileExists -eq $True) 
+ 
+ {Write-Host("Fichier présent dans $pathComplet")}
+
+ else
+ 
+ {Write-Host("Ficher introuvable dans $pathComplet")}
+
+ }
